b8861116754e77dcbdd6ec6fa782fcf22576071f
sanusi-mayowa <sanusifaruq007@gmail.com>
2024-05-05 12:29:47 +0100

    commerce

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Script for navigation bar
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const navbar = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener("click", () => {
+        navbar.classList.add("active")
+    })
+}
+if (close) {
+    close.addEventListener("click", () =>{
+        navbar.classList.remove("active")
+    })
+}
+
+// ================== navbar resposiveness==============
+
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;  
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;  
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;  
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;  
+}
